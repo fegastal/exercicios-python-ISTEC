901962a94ab5ecc90468a7739b2084f81417be4d
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-09 00:03:37 +0000

    Add files via upload

---
+++ lista1-ex1-soma-numeros.py
@@ -0,0 +1,13 @@
+'''
+Resolução do primeiro exercício
+Pedir 2 números ao utilizador, somar e escrever resultado.
+'''
+
+print("Introduza o primeiro número:")
+num1 = int(input())
+print("Introduza o segundo número:")
+num2 = int(input())
+soma = num1 + num2
+print("A soma dos dois números é:", soma)
+print("A soma dos dois números é:{}".format(soma))
+print(f"A soma dos dois números é:{soma}")
